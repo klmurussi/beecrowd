a50f046930febc5c20bc125924bd437c452ed1de
klmurussi <klmurussi@hotmail.com>
2025-10-20 03:42:32 -0300

    Solve 1007: Diferença

---
+++ src/1007.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main () {
+    int A, B, C, D, diferenca;
+	std::cin >> A >> B >> C >> D;
+    diferenca = A * B - C * D;
+    std::cout << "DIFERENCA = " << diferenca << std::endl;
+	return 0;
+}
